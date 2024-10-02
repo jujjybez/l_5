e440968f021124d1eeee6ac03c6bf2e93ade6f21
Appendix Radiant <naebcik@gmail.com>
2024-10-02 15:06:46 +0300

    Add project files.

---
+++ l_5/Program_1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+class MyMatrix
+{
+    private int[,] matrix;
+    private int n, m;
+    public MyMatrix(int n, int m, int min_val, int max_val)
+    {
+        this.n = n;
+        this.m = m;
+        matrix = new int[n, m];
+        Random rand = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++) { matrix[i, j] = rand.Next(min_val, max_val + 1); }
+        }
+    }
+    public void Fill(int min_val, int max_val)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; j++) { matrix[i, j] = rand.Next(min_val, max_val + 1); }
+        }
+    }
+    public int this[int i, int j]
+    {
+        get { return matrix[i, j]; }
+        set { matrix[i, j] = value; }
+    }
+    public MyMatrix ChangeSize(int new_n, int new_m, int min_val, int max_val)
+    {
+        MyMatrix result = new MyMatrix(new_n, new_m, 0, 0);
+        Random rand = new Random();
+        for (int i = 0; i < new_n; i++)
+        {
+            for (int j = 0; j < new_m; j++)
+            {
+                if (i >= n || j >= m) { result.matrix[i, j] = rand.Next(min_val, max_val + 1); }
+                else { result.matrix[i, j] = matrix[i, j]; }
+            }
+        }
+        return result;
+    }
+    public void ShowPartialy(int n, int m)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++) { Console.Write($"{matrix[i, j]} "); }
+            Console.WriteLine();
+        }
+    }
+    public void Show() { ShowPartialy(n, m); }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of rows: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the number of columns: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the minimum value: ");
+        int min_val = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the maximum value: ");
+        int max_val = int.Parse(Console.ReadLine());
+
+        MyMatrix matrix = new MyMatrix(n, m, min_val, max_val);
+
+        Console.WriteLine("Matrix:");
+        matrix.Show();
+
+        Console.WriteLine("\nChanging the matrix size: ");
+        Console.Write("Enter the new number of rows: ");
+        int new_n = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the new number of columns: ");
+        int new_m = int.Parse(Console.ReadLine());
+
+        MyMatrix new_matrix = matrix.ChangeSize(new_n, new_m, min_val, max_val);
+
+        Console.WriteLine("New matrix:");
+        new_matrix.Show();
+
+        Console.WriteLine("\nDisplaying a portion of the matrix: ");
+        matrix.ShowPartialy(2, 1);
+
+        Console.WriteLine("\nChanging an element of the matrix: ");
+        Console.Write("Enter the row index: ");
+        int n_i = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the column index: ");
+        int m_j = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the new value: ");
+        int new_val = int.Parse(Console.ReadLine());
+
+        matrix[n_i, m_j] = new_val;
+
+        Console.WriteLine("Modified matrix:");
+        matrix.Show();
+    }
+}
---
+++ l_5_2/Program_2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+public class MyList<T> : IEnumerable<T>
+{
+    private T[] items;
+    private int count;
+    public MyList()
+    {
+        items = new T[0];
+        count = 0;
+    }
+    public MyList(params T[] val)
+    {
+        items = new T[val.Length];
+        Array.Copy(val, items, val.Length);
+        count = val.Length;
+    }
+    public MyList(IEnumerable<T> collection)
+    {
+        items = new T[0];
+        foreach (var item in collection) { Add(item); }
+    }
+    public void Add(T item)
+    {
+        if (count == items.Length)
+        {
+            int newCapacity = count == 0 ? 4 : count * 2;
+            T[] newItems = new T[newCapacity];
+            Array.Copy(items, newItems, count);
+            items = newItems;
+        }
+        items[count] = item;
+        count++;
+    }
+    public T this[int index]
+    {
+        get { return items[index]; }
+        set { items[index] = value; }
+    }
+    public int Count
+    {
+        get { return count; }
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < count; i++) { yield return items[i]; }
+    }
+    IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyList<int> myList = new MyList<int> { 1, 2, 3, 4, 5 };
+        myList.Add(678);
+        Console.WriteLine($"Elements i - 1: \n");
+        foreach (var item in myList) { Console.WriteLine($"Element: {item}"); }
+        Console.WriteLine($"\nTotal number of elements: {myList.Count}");
+    }
+}
